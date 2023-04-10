4ebda3b8303e2896a03f6e5f4573b34740ad2bf5
inuk082800 <inuk1357@gmail.com>
2023-04-10 12:13:08 +0900

    캐릭터 스와이프 이동까지

---
+++ VR_GameProjectA/VR_GameProject/Assets/Scipts/0410/SampleTileMap.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleTileMap : MonoBehaviour
+{
+    public GameObject tile_001;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int i = 0; i < 20; i++)
+        {
+            for(int j = 0; j< 10; j++)
+            {
+                GameObject temp = (GameObject)Instantiate(tile_001);
+                temp.transform.position = new Vector3(i, 0, j);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
